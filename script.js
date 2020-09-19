2076abb9a93476ede25b4e38a5a1fc97b56eaff8
Beknazar Jumabaev <beknazar.jumabaev_2021@UCA.corp>
2020-09-19 16:31:04 +0600

    first commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("foot01").innerHTML =
 "<p>&copy;  " + new Date().getFullYear() + " Beknazar Jumabaev. All rights reserved.</p>";
 document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
-"<li><a href='indext.html'>Home</a></li>" +
+"<li><a href='index.html'>Home</a></li>" +
 "<li><a href='Data.html'>Data</a></li>" +
 "<li><a href='about.html'>About</a></li>" +
 "<li><a href='References.html'>References</a></li>" +
